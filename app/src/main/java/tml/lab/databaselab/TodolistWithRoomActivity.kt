6768530791e3c7c55b2940d4e18f3f48781cd68e
Tran Manh Linh <devngmo@gmail.com>
2019-08-11 11:12:05 +0700

    + implement add new task in list activity

--- app/src/main/java/tml/lab/databaselab/TodolistWithRoomActivity.kt
+++ app/src/main/java/tml/lab/databaselab/TodolistWithRoomActivity.kt
@@ -9,6 +9,8 @@ import android.view.ViewGroup
 import android.widget.TextView
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -16,27 +18,38 @@ import kotlinx.android.synthetic.main.activity_todolist_with_room.*
 import kotlinx.android.synthetic.main.content_todolist_with_room.*
 import tml.lab.databaselab.UI.TaskRow
 import tml.lab.databaselab.models.TodoTask
+import tml.lab.databaselab.models.TodoTaskViewModel
 
 class TodolistWithRoomActivity : AppCompatActivity() {
-
+    lateinit var taskViewModel: TodoTaskViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_todolist_with_room)
         setSupportActionBar(toolbar)
 
+        val taskListAdapter = TaskAdapter(application)
+        lvTasks.layoutManager = LinearLayoutManager(this)
+        lvTasks.adapter = taskListAdapter
+
+        taskViewModel = ViewModelProviders.of(this).get(TodoTaskViewModel::class.java)
+        taskViewModel.allTasks.observe(this, Observer{
+            tasks -> tasks?.let { taskListAdapter.setTasks(it) }
+        })
+
         fab.setOnClickListener { view ->
             //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-             //   .setAction("Action", null).show()
+            //   .setAction("Action", null).show()
             openTaskEditor_addNew()
         }
-
-        lvTasks.layoutManager = LinearLayoutManager(this)
-        lvTasks.adapter = TaskAdapter(application)
     }
 
     private fun openTaskEditor_addNew() {
         val i = Intent(this, TodoTaskEditorActivity::class.java)
         startActivity(i)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
     class TaskAdapter internal constructor(context:Context)
